678f0d3f8f7de860a1d6877aa0802f236e901531
Daniela Fermin <fermin.daniela@gmail.com>
2021-08-25 12:45:18 +0200

    Change to reacts again

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Daniela reacts
+          Daniela reacts again
         </a>
       </header>
     </div>
